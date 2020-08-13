ef7ef8f5f5eed9010be3fc52440af39122946fa4
Girija Viswanathan <girijavmohan@gmail.com>
2020-08-13 14:27:22 -0700

    Bit manipulations single number with XOR

---
+++ BitManipulations/SingleNumber.js
@@ -0,0 +1,46 @@
+// Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+// You are given an array of integers in which every element appears twice, except for one. Find the element that only appears one time. Your solution should have a linear runtime complexity (O(n)). Try to implement it without using extra memory.
+
+// Example
+
+// For nums = [2, 2, 1], the output should be
+// singleNumber(nums) = 1.
+
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] array.integer nums
+
+// Guaranteed constraints:
+// 1 ≤ nums.length < 104,
+// -109 ≤ nums[i] ≤ 109.
+
+// [output] integer
+
+function singleNumber(nums) {
+    'use strict'
+    let res = 0;
+    for (let num of nums) {
+        res ^= num;
+    }
+    return res;
+  }
+
+  Input:
+nums: [2, 2, 1]
+Output:
+1
+Expected Output:
+1
+Console Output:
+Empty
+
+Input:
+nums: [1]
+Output:
+1
+Expected Output:
+1
+Console Output:
+Empty
