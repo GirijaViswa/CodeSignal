5d25468b29a388f25b6cd6bf33a5b95a377ddb18
Girija Viswanathan <girijavmohan@gmail.com>
2020-09-14 00:13:45 -0700

    corrected version2 of stack

--- DataStructure/MinStack.js
+++ DataStructure/MinStack.js
@@ -1,4 +1,78 @@
 
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+
+
+
+// correct solution
+
+var MinStack = function() {
+    this.stack = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    let min = this.stack.length === 0 ? x : this.stack[this.stack.length - 1].min
+    this.stack.push({val: x, min: Math.min(min, x)})
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    if(this.stack.length > 0){
+        this.stack.pop()
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    if(this.stack.length > 0) {
+        return this.stack[this.stack.length - 1].val
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    if(this.stack.length > 0) {
+        return this.stack[this.stack.length - 1].min
+    }
+};
+
+
+// wrong one down
 
 class MinStack{
     constructor(){
